d3a437c216ffaadc86b455c2907a02a8c2260089
Archita <architasarkar1602@gmail.com>
2025-10-26 22:35:28 +0530

    routes initialized

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,24 @@
 import express from "express";
+import {createServer} from "node:http";
+import {Server} from "socket.io";
+import cors from "cors";
+import { connectToSocket } from "./controllers/socketManager.js";
 
-import dotenv from "dotenv";
 
 const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
+app.get("/home",(req,res)=>{
+    return res.json({message:"API is working"});
+})
+
+app.use(cors({
+    origin:"*",
+    credentials:true
+}));
+
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb",extended:true}));   
 
 export { app };
---
+++ backend/src/controllers/socketManager.js
@@ -0,0 +1,16 @@
+import { Server } from "socket.io";
+
+const connectToSocket = (server) => {
+  const io = new Server(server, {
+    cors: { origin: "*" },
+    });     
+    io.on("connection", (socket) => {               
+        console.log("New client connected: ", socket.id);   
+        socket.on("disconnect", () => {
+            console.log("Client disconnected: ", socket.id);
+        });
+    });
+    return io;
+}     
+
+export { connectToSocket };
